92d83c065c0b2f6cf47fafef9b4b0632db2c0c67
mykel68 <olowookeremicheal9568@gmail.com>
2024-06-02 05:59:29 +0100

    hero done

---
+++ src/components/Hero.svelte
@@ -0,0 +1,42 @@
+<script>
+  import CtAs from "./CTAs.svelte";
+  import Header from "./Header.svelte";
+  import SectionWrapper from "./SectionWrapper.svelte";
+</script>
+
+<SectionWrapper>
+  <Header />
+  <div
+    class="flex flex-col gap-10 flex-1 items-center justify-center pb-10 md:pb-14"
+  >
+    <h2
+      class="text-5xl sm:text-6xl md:text-7xl lg:text-8xl mx-auto w-full text-center max-w-[1200]"
+    >
+      <span class="text-indigo-400">Gym</span>Training
+      <span class="text-slate-600 line-through">Is Hard</span>
+      Just Got <span class="text-indigo-400">Easier</span>
+    </h2>
+    <p
+      class="text-xl sm:text-2xl md:text-3xl text-center mx-auto w-full max-w-[1000px]"
+    >
+      Less thinking, more training.We're the trainer in your pocket to help you
+      <span class="italic">gain muscle, lose fat</span> and build strength.
+    </p>
+    <CtAs />
+    <div class="flex items-center justify-center gap-2 text-base">
+      <p>4.6</p>
+      {#each [1, 2, 3, 4, 5] as index}
+        <div class="grid place-items-center relative">
+          <i class="fa-solid fa-star opacity-0"></i>
+          <div
+            class={"absolute top-0 left-0 grid place-items-center " +
+              (index === 4 ? "w-[60%] overflow-hidden" : "")}
+          >
+            <i class="fa-solid fa-star text-amber-400"></i>
+          </div>
+        </div>
+      {/each}
+      <p>500+</p>
+    </div>
+  </div>
+</SectionWrapper>
---
+++ src/components/Product.svelte
@@ -0,0 +1,36 @@
+<script>
+  import ProductCard from "./ProductCard.svelte";
+  import SectionWrapper from "./SectionWrapper.svelte";
+  let productFeatures = [
+    {
+      featureList: [],
+      description:
+    },
+  ];
+</script>
+
+<SectionWrapper id="product">
+  <div
+    class="'flex flex-col gap-10 sm:gap-14 md:ga24 flex-1 items-center pb-10 md:pb-14"
+  >
+    <div class="flex flex-col gap-2">
+      <p class="opacity-60 text-base sm:text-lg md:text-xl text-center">
+        Start your lifetime <span class="text-indigo-400">fitness journey</span>
+      </p>
+      <h3
+        class="text-3xl sm:text-4xl md:text-5xl mx-auto w-full font-semibold text-center"
+      >
+        Features For You
+      </h3>
+    </div>
+    <div
+      class="grid grid-cols-1 md:grid-cols-2 gap-14 md:gap-14 lg:gap-20 relative text-base"
+    >
+      {#each productFeatures as productFeature, index}
+        <ProductCard {productFeature} {index}>
+          {#if index === 0}{:else if index === 1}{:else}{/if}
+        </ProductCard>
+      {/each}
+    </div>
+  </div>
+</SectionWrapper>
